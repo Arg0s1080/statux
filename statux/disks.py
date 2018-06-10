ac39172f0dea7b2100973ec9b19e757f14c91714
Arg0s1080 <ivan.rincon76@gmail.com>
2018-06-10 17:30:41 +0200

    add docstrings

--- statux/disks.py
+++ statux/disks.py
@@ -37,7 +37,12 @@ def partitions() -> list:
     """Returns a list with partitions"""
     with open(_PARTITIONS, "r") as f:
         stat = f.readlines()
-        return [stat[i].split()[3] for i in range(2, len(stat)) if stat[i].split()[3][-1].isdigit()]
+        res = []
+        for i in range(2, len(stat)):
+            ptt = stat[i].split()[3]
+            if ptt[-1].isdigit():
+                res.append(ptt)
+    return res
 
 
 def mounted_partitions() -> dict:
@@ -83,11 +88,11 @@ def _get_disks_stats():
 def total_size(partition: str, scale="GiB", precision=2):
     """Returns total size of a partition
 
-    Params:
-        partition (str): Partition name (Ex: 'sda1')
-        scale     (str): Return scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+    :Params:
+        :partition (str): Partition name (Ex: 'sda1')
+        :scale     (str): Return scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
                          GiB by default.
-        precision (int): Number of rounding decimals
+        :precision (int): Number of rounding decimals
     """
 
     stat = _get_stat(partition)
@@ -97,10 +102,10 @@ def total_size(partition: str, scale="GiB", precision=2):
 def free_space(partition: str, scale="GiB", precision=2):
     """Returns free partition space
 
-        Params:
-            partition (str): Partition name (Ex: 'sda1')
-            scale     (str): Return scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
-            precision (int): Number of rounding decimals
+        :Params:
+            :partition (str): Partition name (Ex: 'sda1')
+            :scale     (str): Return scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+            :precision (int): Number of rounding decimals
         """
 
     stat = _get_stat(partition)
@@ -110,11 +115,11 @@ def free_space(partition: str, scale="GiB", precision=2):
 def used_space(partition: str, scale="GiB", precision=2):
     """Returns used partition space
 
-        Params:
-            partition (str): Partition name (Ex: 'sda1')
-            scale     (str): Return scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+        :Params:
+            :partition (str): Partition name (Ex: 'sda1')
+            :scale     (str): Return scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
                              GiB by default
-            precision (int): Number of rounding decimals
+            :precision (int): Number of rounding decimals
         """
 
     stat = _get_stat(partition)
@@ -125,62 +130,109 @@ def used_space(partition: str, scale="GiB", precision=2):
 def used_space_percent(partition: str, precision=2) -> float:
     """Returns used partition space percentage
 
-        Params:
-            partition (str): Partition name (Ex: 'sda1')
-            precision (int): Number of rounding decimals
+        :Params:
+            :partition (str): Partition name (Ex: 'sda1')
+            :precision (int): Number of rounding decimals
         """
 
     stat = _get_stat(partition)
     return round((stat.f_blocks - stat.f_bfree) / stat.f_blocks * 100, precision)
 
 
-def _set_delta(*partitions: str, interval=0.0, persecond=False):
+def _set_delta(*partitions_: str, interval=0.0, persecond=False):
     from time import sleep, time
     global _last
     if _last is None or interval > 0.0:
-        # TODO: Delete
-        # print("Debug. Sleeping")
         old_stat = _get_disks_stats()
         sleep(interval)
         elapsed = interval
     else:
-        # TODO: Delete
-        # print("Debug. Not Sleeping")
         old_stat = _last[0]
         elapsed = round(time() - _last[1], 3)  # milliseconds
     new_stat = _get_disks_stats()
     _last = new_stat, time()
     f = {}
-    for partition in partitions:
+    for partition in partitions_:
         read_delta = new_stat[partition][0] - old_stat[partition][0]
         write_delta = new_stat[partition][1] - old_stat[partition][1]
-        res = (read_delta, write_delta) if not persecond else (0.0, 0.0) if not elapsed else \
-            (read_delta / elapsed, write_delta / elapsed)
-        if len(partitions) < 2:
+        res = ((read_delta, write_delta) if not persecond else (0.0, 0.0) if not elapsed else
+               (read_delta / elapsed, write_delta / elapsed))
+        if len(partitions_) < 2:
             return res
         else:
             f[partition] = res
     return f
 
 
-def bytes_read(partition: str, interval=0.0, persecond=False, scale="KiB", precision=2):
-    return set_bytes(_set_delta(partition, interval=interval, persecond=persecond)[0],
+def bytes_read(partition: str, interval=0.0, per_second=False, scale="KiB", precision=2):
+    """Returns bytes read in a partition in a certain time interval
+
+        :Params:
+            :partition  (str):  Partition name (Ex: 'sda1')
+            :interval (float):  Seconds. When value is greater than zero, it returns bytes read
+                                in that period of time. When interval value is 0, it returns
+                                bytes read since the last call
+            :per_second (bool): If it's True returns average value per second
+            :scale      (str):  Output scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+                                KiB by default
+            :precision  (int):  Number of rounding decimal
+    """
+
+    return set_bytes(_set_delta(partition, interval=interval, persecond=per_second)[0],
                      scale_in="bytes", scale_out=scale, precision=precision)
 
 
-def bytes_write(partition: str, interval=0.0, persecond=False, scale="KiB", precision=2):
-    return set_bytes(_set_delta(partition, interval=interval, persecond=persecond)[1],
+def bytes_write(partition: str, interval=0.0, per_second=False, scale="KiB", precision=2):
+    """Returns written bytes in a partition in a certain time interval
+
+        :Params:
+            :partition  (str):  Partition name (Ex: 'sda1')
+            :interval (float):  Seconds. When value is greater than zero, it returns bytes written
+                                in that period of time. When interval value is 0, it returns
+                                bytes read since the last call
+            :per_second (bool): If it's True returns average value per second
+            :scale      (str):  Output scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+                                KiB by default
+            :precision  (int):  Number of rounding decimal
+    """
+
+    return set_bytes(_set_delta(partition, interval=interval, persecond=per_second)[1],
                      scale_in="bytes", scale_out=scale, precision=precision)
 
 
-def bytes_read_write(partition: str, interval=0.0, persecond=False, scale="KiB", precision=2):
-    values = _set_delta(partition, interval=interval, persecond=persecond)
+def bytes_read_write(partition: str, interval=0.0, per_second=False, scale="KiB", precision=2):
+    """Returns a tuple with bytes read and written in a partition in a certain time interval
+
+        :Params:
+            :partition  (str):  Partition name (Ex: 'sda1')
+            :interval (float):  Seconds. When value is greater than zero, it returns bytes read
+                                and written in that period of time. When interval value is 0,
+                                it returns bytes read and written since the last call
+            :per_second (bool): If it's True returns average value per second
+            :scale      (str):  Output scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+                                KiB by default
+            :precision  (int):  Number of rounding decimal
+    """
+
+    values = _set_delta(partition, interval=interval, persecond=per_second)
     return set_bytes(values[0], values[1], scale_in="bytes", scale_out=scale, precision=precision)
 
 
-def bytes_read_write_multi(*partitions: str, interval=0.0, persecond=False, scale="KiB", precision=2):
-    dic = _set_delta(*partitions, interval=interval, persecond=persecond)
+def bytes_read_write_multi(*partitions_: str, interval=0.0, per_second=False, scale="KiB", precision=2):
+    """Returns a dictionary with the bytes read and written in the given partitions.
+
+        :Params:
+            :partitions (str):  Partitions names (Ex: 'sda1','sda2','sda7')
+            :interval (float):  Seconds. When value is greater than zero, it returns bytes read
+                                and written in that period of time. When interval value is 0,
+                                it returns bytes read and written since the last call
+            :per_second (bool): If it's True returns average value per second
+            :scale      (str):  Output scale (bytes, KiB, MiB, GiB, TiB, kB, MB, TB or auto)
+                                KiB by default
+            :precision  (int):  Number of rounding decimal
+    """
+
+    dic = _set_delta(*partitions_, interval=interval, persecond=per_second)
     for key, value in dic.items():
         dic[key] = set_bytes(value[0], value[1], scale_in="bytes", scale_out=scale, precision=precision)
     return dic
-
