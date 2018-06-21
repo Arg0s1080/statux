5a032ac2bfa5be1a0c79d8b9d98f68257625102f
Arg0s1080 <ivan.rincon76@gmail.com>
2018-06-21 18:50:58 +0200

    del non-universal methods. add max_val(). refactor

--- statux/temp.py
+++ statux/temp.py
@@ -15,70 +15,28 @@
 
 
 from os import listdir
-from os.path import join
+from os.path import join, exists
 from statux._conversions import set_celsius
 
-_PKG_INPUT = "/sys/class/thermal/"
-_PARENT = "/sys/devices/platform/"
-_STAT = "hwmon"
+_PARENT = "/sys/devices/platform/coretemp.0/hwmon/"
+_HWMON = "hwmon"
 
 
 def _get_stat():
-    # Only one physical id
     res = {}
-    stat_path = join(_PARENT, "coretemp.0", _STAT)
-    for path in listdir(stat_path):
-        for file in listdir(join(stat_path, path)):
+    if exists(_PARENT):
+        parent_path = _PARENT
+    else:
+        parent_path = "/sys/class/hwmon/"  # in some AMD _PARENT doesn't exist
+    for hwmon in listdir(parent_path):
+        for file in listdir(join(parent_path, hwmon)):
             if file.endswith("label"):
-                path_l = join(stat_path, path, file)
+                path_l = join(parent_path, hwmon, file)
                 with open(path_l, "rb") as file_l:
                     label = file_l.readline()[:-1]
                     with open(path_l.replace("label", "input"), "rb") as file_t:
                         res[label.decode()] = int(file_t.readline()[:-1])
     return res
-
-
-def _get_stat_multi_physical_id():
-    # Experimental: Created to work with more than one physical id
-    res = {}
-    ids = []
-    for coretemp in listdir(_PARENT):
-        if coretemp.startswith("coretemp"):
-            stat_path = join(_PARENT, coretemp, _STAT)
-            for path in listdir(stat_path):
-                cores = {}
-                for file in listdir(join(stat_path, path)):
-                    if file.endswith("label"):
-                        path_l = join(stat_path, path, file)
-                        with open(path_l, "rb") as file_l:
-                            label = file_l.readline()[:-1]
-                            ls = label.split()
-                            if len(ls) > 2 and ls[1] == b"id":
-                                ids.append(int(ls[2]))
-                            # else:  # Uncomment and indent to get core temps only
-                            with open(path_l.replace("label", "input"), "rb") as file_t:
-                                cores[label] = int(file_t.readline()[:-1])
-                for id_ in ids:
-                    res["Physical id %d" % id_] = cores
-    return res
-
-
-def x86_pkg_BAD(scale="celsius", precision=2) -> float:
-    """Returns value of CPU digital temperature package level sensor
-
-    More info: https://www.kernel.org/doc/Documentation/thermal/x86_pkg_temperature_thermal
-
-    :Params:
-        :scale     (str): Return scale Celsius, Fahrenheit, Kelvin or Rankine)
-        :precision (int): Number of rounding decimals
-
-    NOTE: Does not work on many computers
-    """
-    for path in listdir(_PKG_INPUT):
-        if path.startswith("thermal_zone"):
-            with open(join(_PKG_INPUT, path, "type"), "rb") as f:
-                if b"x86_pkg_temp" in f.read():
-                    return set_celsius(float(open(join(_PKG_INPUT, path, "temp")).readline()), scale, precision)
 
 
 def cores(scale="celsius", precision=2) -> list:
@@ -101,33 +59,16 @@ def cpu(scale="celsius", precision=2) -> float:
     """
     stat = _get_stat()
     for key in stat:
-        if not key.startswith("Core"):
+        if " id" in key:
             return set_celsius(stat[key], scale, precision)
 
 
-def cores_multi(scale="celsius", precision=2) -> dict:
-    """Returns a dict with digital thermal sensors values for each core. Each dict key is a physical id
+def max_val(scale="celsius", precision=2) -> float:
+    """Returns the maximum value of the temp sensors obtained
 
     :Params:
         :scale     (str): Return scale Celsius, Fahrenheit, Kelvin or Rankine)
         :precision (int): Number of rounding decimals
-
-    Note: Experimental
     """
-    stat = _get_stat_multi_physical_id()
-    sc = lambda v: set_celsius(v, scale, precision)
-    return {key: [sc(stat[key][k]) for k in sorted(stat[key]) if k.startswith(b"Core")] for key in sorted(stat)}
-
-
-def cpu_multi(scale="celsius", precision=2) -> dict:
-    """Returns a dict with measured values on each surface of the integrated heat spreaders
-
-    :Params:
-        :scale     (str): Return scale Celsius, Fahrenheit, Kelvin or Rankine)
-        :precision (int): Number of rounding decimals
-
-    Note: Experimental
-    """
-    s = _get_stat_multi_physical_id()
-    return {key: [set_celsius(s[key][k], scale, precision) for k in sorted(s[key]) if b"id" in k] for key in sorted(s)}
-
+    stat = _get_stat()
+    return set_celsius(max(stat.values()), scale, precision)
