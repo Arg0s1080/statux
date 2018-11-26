25f21c4e1f897e0a6a6487f5f86fa47ee2aa3349
Arg0s1080 <ivan.rincon76@gmail.com>
2018-11-26 19:31:52 +0100

    add UnexpectedValueError, add ex_handler decorator and change inheritance

--- statux/_errors.py
+++ statux/_errors.py
@@ -14,6 +14,7 @@
 # (ɔ) Iván Rincón 2018
 
 import errno
+from os import strerror
 from os.path import basename
 
 # errno.ENOENT  = 2   # No such file or directory
@@ -23,18 +24,49 @@ from os.path import basename
 # errno.ENOMSG  = 42  # No message of desired type
 
 
-class StatuxError(OSError):
+class StatuxError(Exception):
     pass
 
 
-class ValueNotFoundError(StatuxError):
-    def __init__(self, value, filename, err_no=errno.ENOENT, msg=""):
+class ValueNotFoundError(OSError, StatuxError):
+    def __init__(self, value, filename, err_no, msg=""):
         self.errno = err_no
         self.value = value
         self.filename = filename
-        self.strerror = msg or "%s not found in %s" % (self.value, basename(self.filename))
+        self.strerror = msg or ("%s: %s not found in %s" %
+                                (strerror(self.errno), self.value, basename(self.filename)))
         self.args = (self.errno, self.strerror, self.filename)
-        super(StatuxError, self).__init__(self.errno, self.strerror, self.filename)
+        super(OSError, self).__init__(self.errno, self.strerror, self.filename)
 
     def __repr__(self):
         return str(self.args)
+
+
+class UnexpectedValueError(ValueError, StatuxError):
+    def __init__(self, msg, cause, expected=None):
+        self.cause = cause
+        self.msg = msg
+        self.expected = expected
+        self.args = (self.msg, self.cause, self.expected)
+        super(ValueError, self).__init__(self.msg, self.cause, self.expected)
+
+    def __repr__(self):
+        return str(self.args)
+
+
+def ex_handler(filename, value=""):
+    def raiser(fun):
+        def wrapper(*args, **kwargs):
+            def get_value():
+                return fun.__name__.replace("_", " ")
+            try:
+                return fun(*args, **kwargs)
+            except UnexpectedValueError:
+                raise
+            except FileNotFoundError:
+                raise ValueNotFoundError(value or get_value(), filename, errno.ENOENT, msg=strerror(errno.ENOENT))
+            except ValueError as ex:
+                msg = "%s: %s" % (strerror(errno.ENOMSG), ex.args[0])
+                raise ValueNotFoundError(value or get_value(), filename, errno.ENOMSG, msg=msg)
+        return wrapper
+    return raiser
